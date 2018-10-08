58caff5496c56b3fe9005268814a7cb9d08cea50
dudami43 <dudami43@gmail.com>
2018-10-07 22:38:55 -0300

    pulinho lento e triste

--- main.cpp
+++ main.cpp
@@ -4,19 +4,24 @@
 #include <ctime>
 
 using namespace std;
-int obs_1 = 0, obs_2 = 0, obs_3 = 0, frames = 30, plataforma, cont; 
-GLfloat posX_obstaculos[8], tempo_novo, tempo_antigo, variacao_de_tempo, escalaX = 0, escalaY = 0, posX, posY;
-bool primeiro_desenho = true, rodando = true;
+int frames = 0, plataforma, cont; 
+GLfloat posX_obstaculos[8], posX, posY; // variaveis obstaculos
+GLfloat tempo_novo, tempo_antigo, variacao_de_tempo; // variaveis controle de frames
+GLfloat escalaX = 0, escalaY = 0; // variaveis "camera"
+GLfloat tempo_pulo, tempo_inicial, t; // variaveis pulo
+GLfloat posX_jogador = 0, posY_jogador = 4; // variaveis jogador
+bool primeiro_desenho = true, rodando = true, pulando = false;
 
 bool posicaoValida(GLfloat posicao)
 {
     
-    if(posicao>980 && posicao<=1000) return false; // estar logo antes da subida de plataforma
+    if(posicao>960 && posicao<=1000) return false; // estar logo antes da subida de plataforma
     if(posicao>1088) return false; // estar depois da linha de chegada
-    if(posicao<20) return false; // estar muito no inicio
+    if(posicao<50) return false; // estar muito no inicio
+    if(posicao>1000 && posicao<1050) return false; // estar muito no inicio da plataforma nivel 1
     for(int i = 0; i< cont; i++) // estar muito próximo de algum dos outros
     {
-        if(posicao>(posX_obstaculos[i]-30) && posicao<(posX_obstaculos[i]+30)) return false;
+        if(posicao>(posX_obstaculos[i]-50) && posicao<(posX_obstaculos[i]+50)) return false;
     }
     return true;
 }
@@ -24,7 +29,6 @@ bool posicaoValida(GLfloat posicao)
 void geraPosicaoObstaculo()
 {
     plataforma = rand() % 2;
-    //cout << "Plat " << plataforma; 
     if(!plataforma)
     {
         posX = (rand() % 100)*10;
@@ -40,6 +44,7 @@ void geraPosicaoObstaculo()
 void criaObstaculo()
 {
     cont = 0;
+    int obs_1 = 0, obs_2 = 0, obs_3 = 0;
     if(primeiro_desenho)
     {
         primeiro_desenho = false;
@@ -50,7 +55,6 @@ void criaObstaculo()
             {
                 geraPosicaoObstaculo();
             }  
-            cout << " PX " << posX << endl; 
             glBegin(GL_TRIANGLES);
                 glVertex2f(posX, posY);
                 glVertex2f(posX+10, posY);
@@ -67,7 +71,6 @@ void criaObstaculo()
             {
                 geraPosicaoObstaculo();
             }  
-            cout << " PX " << posX << endl; 
             glBegin(GL_TRIANGLES);
                 glVertex2f(posX, posY);
                 glVertex2f(posX+10, posY);
@@ -89,7 +92,6 @@ void criaObstaculo()
             {
                 geraPosicaoObstaculo();
             }  
-            cout << " PX " << posX << endl; 
             glBegin(GL_TRIANGLES);
                 glVertex2f(posX, posY);
                 glVertex2f(posX+10, posY);
@@ -192,6 +194,7 @@ void criaObstaculo()
     
     
 }
+
 void criaPlataforma()
 {
     glColor3f (0.30, 0.79, 1.0);
@@ -223,22 +226,110 @@ void criaPlataforma()
 
 }
 
+void criaJogador()
+{
+    glColor3f (0.5, 0.0, 0.5);
+    glBegin(GL_POLYGON);
+        glVertex2f(posX_jogador, posY_jogador);
+        glVertex2f(posX_jogador+10, posY_jogador);
+        glVertex2f(posX_jogador+10, posY_jogador+5);
+        glVertex2f(posX_jogador, posY_jogador+5);
+    glEnd();
+    glColor3f (1.0, 1.0, 1.0);
+    glBegin(GL_POLYGON);
+        glVertex2f(posX_jogador, posY_jogador+5);
+        glVertex2f(posX_jogador+10, posY_jogador+5);
+        glVertex2f(posX_jogador+10, posY_jogador+10);
+        glVertex2f(posX_jogador, posY_jogador+10);
+    glEnd();
+}
+
+void puloJogador()
+{ 
+    tempo_pulo = glutGet(GLUT_ELAPSED_TIME)/1000.0; 
+    t = tempo_pulo - tempo_inicial;
+    if(t<(2.0*(24.0/9.8)))
+    {
+        posY_jogador = 4.0 + (24.0*t) - ((0.5)*(9.8)*(t*t));
+        tempo_pulo = glutGet(GLUT_ELAPSED_TIME)/1000.0; 
+        t = tempo_pulo - tempo_inicial;
+    }
+}
+
+bool colisao_obstaculo()
+{
+    int i=0;
+    if(posX_jogador>=1000 && posY_jogador<=15) return true;
+    while(i<2)
+    {
+        if(posX_jogador>=posX_obstaculos[i] && posX_jogador<=posX_obstaculos[i]+10 && (posY_jogador<=10))
+        {
+            return true;
+        }
+        i++;
+    }
+    while(i<5)
+    {
+        if(posX_jogador>=posX_obstaculos[i] && posX_jogador<=posX_obstaculos[i]+20 && posY_jogador<=10)
+        {
+            return true;
+        }
+        i++;
+    }
+    while(i<8)
+    {
+        if(posX_jogador>=posX_obstaculos[i] && posX_jogador<=posX_obstaculos[i]+10 && posY_jogador<=20)
+        {
+            return true;
+        }
+        i++;
+    }
+    return false;
+}
+
+bool chegou()
+{
+    if(posX_jogador>=1088) return true;
+    return false;
+}
+
+void handleKeyboard(unsigned char key, int x, int y)
+{
+	switch (key) {
+		case 32:
+            tempo_inicial = glutGet(GLUT_ELAPSED_TIME)/1000.0; 
+            pulando = true;
+        break;
+	}
+}
+
 void display(void)
 {
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    gluOrtho2D(0 + escalaX, 200 + escalaX, -20 + escalaY, 70 + escalaY);
-    glClear (GL_COLOR_BUFFER_BIT);
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
-    criaPlataforma();
-    criaObstaculo();
-    //cout << "display\n";
-    escalaX += 0.1;
-    //cout << escalaX << endl;
-    glutSwapBuffers();  
-    glutPostRedisplay();
-
+    if(rodando)
+    {
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+        gluOrtho2D(-100 + escalaX, 100 + escalaX, -20 + escalaY, 70 + escalaY);
+        glClear (GL_COLOR_BUFFER_BIT);
+        glMatrixMode(GL_MODELVIEW);
+        glLoadIdentity();
+        criaPlataforma();
+        criaObstaculo();
+        criaJogador();
+        escalaX += 0.01;
+        if(pulando) puloJogador();
+        posX_jogador += 0.01;
+        /*if(colisao_obstaculo())
+        {
+            cout << "colidiu" << endl;
+        }
+        if(chegou())
+        {
+            cout << "chegou" << endl;
+        }*/
+        glutSwapBuffers();  
+        glutPostRedisplay();
+    }
 }
 
 void init (void)
@@ -246,7 +337,6 @@ void init (void)
     glClearColor (0.0, 0.0, 0.0, 0.0);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    gluOrtho2D(0 + escalaX, 200 + escalaX, -20 + escalaY, 70 + escalaY);
 }
 
 void novoMainLoop(){
@@ -273,7 +363,7 @@ int main(int argc, char** argv)
     glutInitWindowPosition (300, 10);
     glutCreateWindow ("Trabalho 2");
     init ();
-    //glutKeyboardFunc(handle_key);
+    glutKeyboardFunc(handleKeyboard);
     //glutMouseFunc(handle_mouse);
     glutDisplayFunc(display);
     novoMainLoop();
